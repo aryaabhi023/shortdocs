6b1fc2aa5a5c7fbf32ca727d0eabace51ad9d52c
Abhishek Arya <abhishekparmar2306@gmail.com>
2025-09-19 12:26:36 +0530

    add confirmation dialog for document deletion

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -12,6 +12,7 @@ export default function File({ params }) {
     const router = useRouter();
     const [document, setDocument] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [display, setDisplay] = useState(false);
 
     useEffect(() => {
         if (!id) return;
@@ -48,13 +49,35 @@ export default function File({ params }) {
 
     return (
         <div className="bg-gradient-to-r from-[#c7d2fe] via-white to-[#fbcfe8] min-h-screen px-4 py-16">
+                {display && 
+                    <div className="fixed inset-0 bg-white/70 bg-opacity-50 flex items-center justify-center z-50">
+                        <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm text-center">
+                            Are You Sure You Want To Delete This Document?
+                            <div className="mt-4 flex justify-center gap-4">
+                                <button>
+                                    <span
+                                        className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+                                        onClick={handleClick}
+                                    > Yes </span>
+                                </button>
+                                <button>
+                                    <span
+                                        className="bg-gray-300 text-gray-700 px-4 py-2 rounded hover:bg-gray-400"
+                                        onClick={() => setDisplay(false)}
+                                    > No 
+                                    </span>
+                                </button>
+                            </div>  
+                        </div>
+                    </div>
+                }
             <div className="relative max-w-5xl mx-auto bg-white rounded-2xl shadow-lg p-6 space-y-4 border border-orange-200">
 
                 {user && user.email === document?.email && (
                     <>
                         <button
                             className="cursor-pointer absolute md:top-4 md:right-4 top-2 right-2"
-                            onClick={handleClick}
+                            onClick={() => setDisplay(true)}
                         >
                             ❌
                         </button>
